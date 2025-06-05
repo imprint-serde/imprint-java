32640cdb551ddd9dd137f0c21be4973dac5234ad
expand3d <>
2025-06-05 16:12:02 -0400

    ok plugin didn't work apparently so reverting that and just reducing Comparison tests iterations manually

--- src/jmh/java/com/imprint/benchmark/ComparisonBenchmark.java
+++ src/jmh/java/com/imprint/benchmark/ComparisonBenchmark.java
@@ -38,8 +38,8 @@ import java.util.concurrent.TimeUnit;
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @State(Scope.Benchmark)
-@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
+@Warmup(iterations = 2, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
 @Fork(1)
 @SuppressWarnings("unused")
 public class ComparisonBenchmark {
